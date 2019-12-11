21e59f5202c0113d0c73b1f31ad10879089c9518
Sebastian Wiendl <48670424+sewiendl@users.noreply.github.com>
2019-12-11 09:35:51 +0100

    Adds debug output

--- .travis/test-pyenv.sh
+++ .travis/test-pyenv.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -ev
 
-cd ./src
-pipenv lock --requirements > req.txt
-cd ..
-
-key_var=AWS_ACCESS_KEY_ID_${DEPLOYMENT_STAGE^^}
-sed -i -e "s|AWS_ACCESS_KEY_ID|${!key_var}|g" $HOME/.aws/credentials
-
-secret_var=AWS_SECRET_ACCESS_KEY_${DEPLOYMENT_STAGE^^}
-sed -i -e "s|AWS_SECRET_ACCESS_KEY|${!secret_var}|g" $HOME/.aws/credentials
-
-sls deploy -s $DEPLOYMENT_STAGE --force --aws-profile $DEPLOYMENT_STAGE --app-version $TRAVIS_TAG
+pyenv versions
